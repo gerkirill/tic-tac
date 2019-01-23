ed01618a5b0c4d0d4a71cff11b6772fcced3c611
Stanis Amnell <StanislavTopchyi@gmail.com>
2019-01-23 20:41:38 +0200

    STAS

--- test/get-phase.spec.js
+++ test/get-phase.spec.js
@@ -2,27 +2,48 @@ const assert = require('assert');
 const getPhase = require('../js/get-phase.js');
 
 describe('#getPhase()', function() {
-
-  xit('should be a function', function() {
+  var board0 = [
+    '-', '-', '0',
+    '-', 'X', '-',
+    '-', '-', '-',
+  ];
+  it('should be a function', function() {
     assert.deepEqual(typeof getPhase, 'function');
   })
 
-  xit('should return "progress" when the game is in progress', function() {
-    
+  var board1 = [
+    '-', '-', '0',
+    '-', 'X', '-',
+    '-', '-', '-',
+  ];
+  it('should return "progress" when the game is in progress', function() {
+    assert.deepEqual(getPhase(board1), 'progress');
   })
 
-  xit('should return "x_win" if "x" player wins', function() {
-    
+  var board2 = [
+    '-', 'X', '0',
+    '-', 'X', '-',
+    '-', 'X', '-',
+  ];
+  it('should return "x_win" if "x" player wins', function() {
+    assert.deepEqual(getPhase(board2), 'x_win');
   })
 
-  xit('should return "o_win" if "o" player wins', function() {
-    
+  var board3 = [
+    '0', '0', '0',
+    '-', 'X', '-',
+    '-', 'X', '-',
+  ];
+  it('should return "o_win" if "o" player wins', function() {
+    assert.deepEqual(getPhase(board3), 'o_win');
   })
 
-  xit('should return "draw" in a case of draw', function() {
-    
+  var board4 = [
+    '0', '0', 'X',
+    'X', 'X', '0',
+    '0', '0', 'X',
+  ];
+  it('should return "draw" in a case of draw', function() {
+    assert.deepEqual(getPhase(board4), 'draw');
   })
-
-  
-
 })
