872b3e0fdaac7d7eded2c9f3f09183f1f242123d
BIlous Alexander <d818651@gmail.com>
2019-01-23 23:20:10 +0200

    get-phase.js Bilous solution

--- js/get-phase.js
+++ js/get-phase.js
@@ -1,34 +1,16 @@
 function getPhase(board) {
-  board = board.slice();
-  var resultArr = [];
-  for (var i = 0; i < board.length; i++) {
-    if (board[i] == 'x') {board[i] = 1;}
-    else if (board[i] == 'o') {board[i] = -1;} 
-    else {board[i] = 0;}
+
+  for (var i = 0; i <= 2; i++) {
+    if (board[i] == board[3 + i] && board[i] == board[6 + i] && board[i] != '-') return board[i] + '_win';
+    if (board[i * 3] == board[1 + i * 3] && board[i * 3] == board[2 + i * 3] && board[i * 3] != '-') return board[i * 3] + '_win';
   }
-  resultArr[0] = board[0] + board[1] + board[2];
-  resultArr[1] = board[3] + board[4] + board[5];
-  resultArr[2] = board[6] + board[7] + board[8];
-  resultArr[3] = board[0] + board[3] + board[6];
-  resultArr[4] = board[1] + board[4] + board[7];
-  resultArr[5] = board[2] + board[5] + board[8];
-  resultArr[6] = board[0] + board[4] + board[8];
-  resultArr[7] = board[2] + board[4] + board[6];
-  var result;
-  if (board.includes(0)) {
-    result = 'progress';
-  } else {
-    result = 'draw';
-  }
-  if (resultArr.includes(-3)) {
-    result = 'o_win';
-  } else if (resultArr.includes(3)) {
-    result = 'x_win';
-  }
-  return result;
+  if (board[0] == board[4] && board[0] == board[8] && board[0] != '-') return board[0] + '_win';
+  if (board[2] == board[4] && board[2] == board[6] && board[2] != '-') return board[2] + '_win';
+
+  if (board.indexOf('-') != -1) return 'progress'; else
+
+    return 'draw';
 }
-  // write your code here
-  // possible return values: 'progress', 'draw' 'x_win', 'o_win'
 
 // DON't modify below this line !!!
 typeof module !== 'undefined' && (module.exports = getPhase);
