5ac4031084b53fa60b369d899307feb8a3a731ce
BIlous Alexander <d818651@gmail.com>
2019-01-23 20:14:43 +0200

    get-phase.js second

--- js/get-phase.js
+++ js/get-phase.js
@@ -5,7 +5,7 @@ function getPhase(board) {
   arr[2] = [board[6], board[7], board[8]];
 
   for (var i = 0; i <= 2; i++) {
-    if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2]) return arr[i][0] + '_win';
+    if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2] && a[i][0]!='-') return arr[i][0] + '_win';
   }
 
   // if (arr[0][0]==arr[0][1] && arr[0][0]==arr[0][2]) return arr[0][0]+'_win'; else
@@ -13,17 +13,18 @@ function getPhase(board) {
   // if (arr[2][0]==arr[2][1] && arr[2][0]==arr[2][2]) return arr[2][0]+'_win'; else
 
   for (var i = 0; i <= 2; i++) {
-    if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i]) return arr[0][i] + '_win';
+    if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i] && a[0][i]!='-') return arr[0][i] + '_win';
   }
 
   // if (arr[0][0]==arr[1][0] && arr[1][0]==arr[2][0]) return arr[0][0]+'_win'; else
   // if (arr[0][1]==arr[1][1] && arr[0][1]==arr[2][1]) return arr[0][1]+'_win'; else 
   // if (arr[0][2]==arr[1][2] && arr[1][2]==arr[2][2]) return arr[0][2]+'_win'; else
 
-  if (arr[0][0] == arr[1][4] && arr[0][0] == arr[2][2]) return arr[0][0] + '_win';
-  if (arr[0][2] == arr[1][4] && arr[0][2] == arr[2][0]) return arr[0][2] + '_win';
+  if (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]) return arr[0][0] + '_win';
+  if (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0]) return arr[0][2] + '_win';
 
   if (board.indexOf('0') > 0) return 'progress'; else
+  
     return 'draw';
 
   // write your code here
