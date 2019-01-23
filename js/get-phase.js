61cff5aee8a90a1c459f848c81ebc4d80e58a85d
Stanis Amnell <StanislavTopchyi@gmail.com>
2019-01-23 20:53:19 +0200

    Stas for phase

--- js/get-phase.js
+++ js/get-phase.js
@@ -1,37 +1,33 @@
 function getPhase(board) {
-  var arr = [];
-  arr[0] = [board[0], board[1], board[2]];
-  arr[1] = [board[3], board[4], board[5]];
-  arr[2] = [board[6], board[7], board[8]];
-
-  for (var i = 0; i <= 2; i++) {
-    if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2] && a[i][0]!='-') return arr[i][0] + '_win';
+  var resultArr = [];
+  for (var i = 0; i < board.length; i++) {
+    if (board[i] == 'X') {board[i] = 1;}
+    else if (board[i] == '0') {board[i] = -1;} 
+    else {board[i] = 0;}
   }
-
-  // if (arr[0][0]==arr[0][1] && arr[0][0]==arr[0][2]) return arr[0][0]+'_win'; else
-  // if (arr[1][0]==arr[1][1] && arr[1][0]==arr[1][2]) return arr[1][0]+'_win'; else
-  // if (arr[2][0]==arr[2][1] && arr[2][0]==arr[2][2]) return arr[2][0]+'_win'; else
-
-  for (var i = 0; i <= 2; i++) {
-    if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i] && a[0][i]!='-') return arr[0][i] + '_win';
+  resultArr[0] = board[0] + board[1] + board[2];
+  resultArr[1] = board[3] + board[4] + board[5];
+  resultArr[2] = board[6] + board[7] + board[8];
+  resultArr[3] = board[0] + board[3] + board[6];
+  resultArr[4] = board[1] + board[4] + board[7];
+  resultArr[5] = board[2] + board[5] + board[8];
+  resultArr[6] = board[0] + board[4] + board[8];
+  resultArr[7] = board[2] + board[4] + board[6];
+  var result;
+  if (board.includes(0)) {
+    result = 'progress';
+  } else {
+    result = 'draw';
   }
-
-  // if (arr[0][0]==arr[1][0] && arr[1][0]==arr[2][0]) return arr[0][0]+'_win'; else
-  // if (arr[0][1]==arr[1][1] && arr[0][1]==arr[2][1]) return arr[0][1]+'_win'; else 
-  // if (arr[0][2]==arr[1][2] && arr[1][2]==arr[2][2]) return arr[0][2]+'_win'; else
-
-  if (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]) return arr[0][0] + '_win';
-  if (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0]) return arr[0][2] + '_win';
-
-  if (board.indexOf('0') > 0) return 'progress'; else
-  
-    return 'draw';
-
+  if (resultArr.includes(-3)) {
+    result = 'o_win';
+  } else if (resultArr.includes(3)) {
+    result = 'x_win';
+  }
+  return result;
+}
   // write your code here
   // possible return values: 'progress', 'draw' 'x_win', 'o_win'
-}
-//    x 0 x     x 0 0
-//    0 x 0     0 x 0
-//    x 0 0     x 0 x
+
 // DON't modify below this line !!!
 typeof module !== 'undefined' && (module.exports = getPhase);
